8e61ea45d697513b491a81c8fa807b5c37bcfdfd
Kuiliang Zhang <z1219202167@outlook.com>
2025-07-12 12:08:34 -0700

    fix: rollup version

--- packages/db/tsup.config.ts
+++ packages/db/tsup.config.ts
@@ -1,18 +1,9 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['src/index.ts', 'src/client.ts'],
+  entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
   dts: true,
   clean: true,
-  splitting: false,
-  sourcemap: true,
-  outDir: 'dist',
-  tsconfig: './tsconfig.json',
-  esbuildOptions(options) {
-    // 将路径别名转换为相对路径
-    options.alias = {
-      '@': './src',
-    }
-  },
+  shims: true,
 })
