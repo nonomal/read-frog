2de434b9b32433b87e0f577ff2173028a6ca50e3
Kuiliang Zhang <z1219202167@outlook.com>
2025-07-14 00:38:39 -0700

    fix: add skip env validation in turbo

--- apps/website/src/env.js
+++ apps/website/src/env.js
@@ -12,6 +12,10 @@ export const env = createEnv({
   },
   client: {},
   experimental__runtimeEnv: {},
+  /**
+   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
+   * useful for Docker builds.
+   */
   skipValidation: !!process.env.SKIP_ENV_VALIDATION,
   emptyStringAsUndefined: true,
 })
